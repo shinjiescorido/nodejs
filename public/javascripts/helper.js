a395da02b7106faa84fcaaa8fd427d0216d41884
Shinji Escorido <shinji.escorido@gmail.com>
2013-09-12 19:13:06 +0800

    project pushed

---
+++ public/javascripts/helper.js
@@ -0,0 +1,51 @@
+$(document).ready(function($){
+
+
+		if($('#errmsgs').html() == ''){
+				$('#errmsgs').hide();
+		}
+
+
+});
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(emailAddress);
+};
+function validater(o){
+
+	if($('#email').val() == ''){
+	$('#errmsgs').html('mail required');
+	$('#errmsgs').show();
+		return false;
+	}
+
+	if(!isValidEmailAddress($('#email').val())){
+	$('#errmsgs').html('invalid email address');
+	$('#errmsgs').show();
+		return false;
+	}
+
+		if($('#pword').val() == ''){
+	$('#errmsgs').html('password required');
+	$('#errmsgs').show();
+		return false;
+	}
+
+	return true;
+}
+function postvalidater(o){
+
+	if($('#title').val() == ''){
+	$('#errmsgs').html('title required');
+	$('#errmsgs').show();
+		return false;
+	}
+
+		if($('#content').val() == ''){
+	$('#errmsgs').html('content required');
+	$('#errmsgs').show();
+		return false;
+	}
+
+	return true;
+}
